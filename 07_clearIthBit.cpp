661f985a58449dd2c4d62c590a8de70d528c5431
Devolena <44612671+Devolena@users.noreply.github.com>
2020-10-31 15:18:31 +0530

    Add files via upload

---
+++ 07_clearIthBit.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+void clearithbit(int &n,int i){
+	int a = 1;
+	a = a<<i;
+	a = ~a;
+	// we are passing by reference so we don't need to return anythink we are pointing to same location as n;
+	n = n & a;
+}
+
+int main(){
+	int n = 5,i=0;
+	clearithbit(n,i);
+	cout<<n<<endl;
+	return 0;
+}
