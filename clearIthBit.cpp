457134b0e1bafacf6fe7c3aa554fb6e51eb1780c
Devolena <44612671+Devolena@users.noreply.github.com>
2020-10-31 15:22:12 +0530

    Update and rename 07_clearIthBit.cpp to clearIthBit.cpp

--- clearIthBit.cpp
+++ clearIthBit.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void clearithbit(int &n,int i){
 	int a = 1;
+	// Creating bitmask for clearing ith bit
 	a = a<<i;
 	a = ~a;
 	// we are passing by reference so we don't need to return anythink we are pointing to same location as n;
